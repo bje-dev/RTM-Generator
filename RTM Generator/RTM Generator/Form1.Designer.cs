44e94542bc647456737426901a0966f606d0daef
Bernardo Espinoza <bernardo.julian.espinoza@gmail.com>
2023-06-16 10:54:57 -0300

    network parameters

--- RTM Generator/RTM Generator/Form1.Designer.cs
+++ RTM Generator/RTM Generator/Form1.Designer.cs
@@ -28,13 +28,299 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.comboBox8 = new System.Windows.Forms.ComboBox();
+            this.comboBox9 = new System.Windows.Forms.ComboBox();
+            this.comboBox10 = new System.Windows.Forms.ComboBox();
+            this.comboBox11 = new System.Windows.Forms.ComboBox();
+            this.comboBox12 = new System.Windows.Forms.ComboBox();
+            this.comboBox13 = new System.Windows.Forms.ComboBox();
+            this.comboBox14 = new System.Windows.Forms.ComboBox();
+            this.comboBox15 = new System.Windows.Forms.ComboBox();
+            this.comboBox16 = new System.Windows.Forms.ComboBox();
+            this.comboBox17 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(379, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(560, 27);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "OTA";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(230, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(521, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "/";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(110, 133);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(55, 21);
+            this.comboBox2.TabIndex = 25;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(171, 133);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(53, 21);
+            this.comboBox3.TabIndex = 26;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(230, 133);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(52, 21);
+            this.comboBox4.TabIndex = 27;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(288, 133);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(52, 21);
+            this.comboBox5.TabIndex = 28;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(110, 172);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(55, 21);
+            this.comboBox6.TabIndex = 29;
+            // 
+            // comboBox7
+            // 
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.Location = new System.Drawing.Point(171, 171);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(53, 21);
+            this.comboBox7.TabIndex = 30;
+            // 
+            // comboBox8
+            // 
+            this.comboBox8.FormattingEnabled = true;
+            this.comboBox8.Location = new System.Drawing.Point(230, 172);
+            this.comboBox8.Name = "comboBox8";
+            this.comboBox8.Size = new System.Drawing.Size(52, 21);
+            this.comboBox8.TabIndex = 31;
+            // 
+            // comboBox9
+            // 
+            this.comboBox9.FormattingEnabled = true;
+            this.comboBox9.Location = new System.Drawing.Point(288, 172);
+            this.comboBox9.Name = "comboBox9";
+            this.comboBox9.Size = new System.Drawing.Size(52, 21);
+            this.comboBox9.TabIndex = 32;
+            // 
+            // comboBox10
+            // 
+            this.comboBox10.FormattingEnabled = true;
+            this.comboBox10.Location = new System.Drawing.Point(110, 212);
+            this.comboBox10.Name = "comboBox10";
+            this.comboBox10.Size = new System.Drawing.Size(55, 21);
+            this.comboBox10.TabIndex = 33;
+            // 
+            // comboBox11
+            // 
+            this.comboBox11.FormattingEnabled = true;
+            this.comboBox11.Location = new System.Drawing.Point(171, 212);
+            this.comboBox11.Name = "comboBox11";
+            this.comboBox11.Size = new System.Drawing.Size(53, 21);
+            this.comboBox11.TabIndex = 34;
+            // 
+            // comboBox12
+            // 
+            this.comboBox12.FormattingEnabled = true;
+            this.comboBox12.Location = new System.Drawing.Point(230, 212);
+            this.comboBox12.Name = "comboBox12";
+            this.comboBox12.Size = new System.Drawing.Size(52, 21);
+            this.comboBox12.TabIndex = 35;
+            // 
+            // comboBox13
+            // 
+            this.comboBox13.FormattingEnabled = true;
+            this.comboBox13.Location = new System.Drawing.Point(289, 212);
+            this.comboBox13.Name = "comboBox13";
+            this.comboBox13.Size = new System.Drawing.Size(51, 21);
+            this.comboBox13.TabIndex = 36;
+            // 
+            // comboBox14
+            // 
+            this.comboBox14.FormattingEnabled = true;
+            this.comboBox14.Location = new System.Drawing.Point(110, 277);
+            this.comboBox14.Name = "comboBox14";
+            this.comboBox14.Size = new System.Drawing.Size(55, 21);
+            this.comboBox14.TabIndex = 37;
+            // 
+            // comboBox15
+            // 
+            this.comboBox15.FormattingEnabled = true;
+            this.comboBox15.Location = new System.Drawing.Point(171, 276);
+            this.comboBox15.Name = "comboBox15";
+            this.comboBox15.Size = new System.Drawing.Size(53, 21);
+            this.comboBox15.TabIndex = 38;
+            // 
+            // comboBox16
+            // 
+            this.comboBox16.FormattingEnabled = true;
+            this.comboBox16.Location = new System.Drawing.Point(230, 277);
+            this.comboBox16.Name = "comboBox16";
+            this.comboBox16.Size = new System.Drawing.Size(52, 21);
+            this.comboBox16.TabIndex = 39;
+            // 
+            // comboBox17
+            // 
+            this.comboBox17.FormattingEnabled = true;
+            this.comboBox17.Location = new System.Drawing.Point(289, 276);
+            this.comboBox17.Name = "comboBox17";
+            this.comboBox17.Size = new System.Drawing.Size(51, 21);
+            this.comboBox17.TabIndex = 40;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(108, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(222, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(368, 278);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(51, 20);
+            this.textBox3.TabIndex = 41;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(415, 47);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 42;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(549, 47);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 43;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox17);
+            this.Controls.Add(this.comboBox16);
+            this.Controls.Add(this.comboBox15);
+            this.Controls.Add(this.comboBox14);
+            this.Controls.Add(this.comboBox13);
+            this.Controls.Add(this.comboBox12);
+            this.Controls.Add(this.comboBox11);
+            this.Controls.Add(this.comboBox10);
+            this.Controls.Add(this.comboBox9);
+            this.Controls.Add(this.comboBox8);
+            this.Controls.Add(this.comboBox7);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.ComboBox comboBox8;
+        private System.Windows.Forms.ComboBox comboBox9;
+        private System.Windows.Forms.ComboBox comboBox10;
+        private System.Windows.Forms.ComboBox comboBox11;
+        private System.Windows.Forms.ComboBox comboBox12;
+        private System.Windows.Forms.ComboBox comboBox13;
+        private System.Windows.Forms.ComboBox comboBox14;
+        private System.Windows.Forms.ComboBox comboBox15;
+        private System.Windows.Forms.ComboBox comboBox16;
+        private System.Windows.Forms.ComboBox comboBox17;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
--- RTM Generator/RTM Generator/Form1.cs
+++ RTM Generator/RTM Generator/Form1.cs
@@ -3,18 +3,264 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace RTM_Generator
 {
     public partial class Form1 : Form
     {
+        private ArduinoINOGenerator generator;
+
         public Form1()
         {
             InitializeComponent();
+            generator = new ArduinoINOGenerator();
+
+            int inicio = 0;
+            int fin = 255;
+
+            // Agrega los números al ComboBox
+            for (int i = inicio; i <= fin; i++)
+            {
+                comboBox2.Items.Add(i);
+                comboBox3.Items.Add(i);
+                comboBox4.Items.Add(i);
+                comboBox5.Items.Add(i);
+                comboBox6.Items.Add(i);
+                comboBox7.Items.Add(i);
+                comboBox8.Items.Add(i);
+                comboBox9.Items.Add(i);
+                comboBox10.Items.Add(i);
+                comboBox11.Items.Add(i);
+                comboBox12.Items.Add(i);
+                comboBox13.Items.Add(i);
+                comboBox14.Items.Add(i);
+                comboBox15.Items.Add(i);
+                comboBox16.Items.Add(i);
+                comboBox17.Items.Add(i);
+
+
+            }
+
+            textBox3.Text = "80";
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nameNetwork = textBox1.Text as string;
+            string passNetwork = textBox2.Text as string;
+
+            string ip1 = comboBox2.SelectedItem as string;
+            string ip2 = comboBox3.SelectedItem as string;
+            string ip3 = comboBox4.SelectedItem as string;
+            string ip4 = comboBox5.SelectedItem as string;
+
+            string mask1 = comboBox6.SelectedItem as string;
+            string mask2 = comboBox7.SelectedItem as string;
+            string mask3 = comboBox8.SelectedItem as string;
+            string mask4 = comboBox9.SelectedItem as string;
+
+            string gate1 = comboBox10.SelectedItem as string;
+            string gate2 = comboBox11.SelectedItem as string;
+            string gate3 = comboBox12.SelectedItem as string;
+            string gate4 = comboBox13.SelectedItem as string;
+
+            string server1 = comboBox14.SelectedItem as string;
+            string server2 = comboBox15.SelectedItem as string;
+            string server3 = comboBox16.SelectedItem as string;
+            string server4 = comboBox17.SelectedItem as string;
+
+            string portServer = textBox3.Text as string;
+
+            string km = textBox4.Text as string;
+            string kmDec = textBox5.Text as string;
+
+
+            if (string.IsNullOrEmpty(nameNetwork))
+            {
+                MessageBox.Show("Indica el SSID de la red WIFI");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(passNetwork))
+            {
+                MessageBox.Show("Introduce la contraseña de la red WIFI");
+                return;
+            }
+
+
+            if (string.IsNullOrEmpty(ip1)|| string.IsNullOrEmpty(ip2)|| string.IsNullOrEmpty(ip3)|| string.IsNullOrEmpty(ip4))
+            {
+                MessageBox.Show("Debe completar correctamente los campos de IP del dispositivo.");
+                return;
+            }
+            if (string.IsNullOrEmpty(mask1) || string.IsNullOrEmpty(mask2) || string.IsNullOrEmpty(mask3) || string.IsNullOrEmpty(mask4))
+            {
+                MessageBox.Show("Debe completar correctamente los campos de MASCARA del dispositivo.");
+                return;
+            }
+            if (string.IsNullOrEmpty(gate1) || string.IsNullOrEmpty(gate2) || string.IsNullOrEmpty(gate3) || string.IsNullOrEmpty(gate4))
+            {
+                MessageBox.Show("Debe completar correctamente los campos de PUERTA DE ENLACE del dispositivo.");
+                return;
+            }
+            if (string.IsNullOrEmpty(server1) || string.IsNullOrEmpty(server2) || string.IsNullOrEmpty(server3) || string.IsNullOrEmpty(server4))
+            {
+                MessageBox.Show("Debe completar correctamente los campos del SERVIDOR donde apunta.");
+                return;
+            }
+
+            string cppCode = @"
+            const char* ssid = ""{paramNameNetwork}"";
+            const char* password = ""{paramPassNetwork}"";
+            const char* host = ""{paramServ1}.{paramServ2}.{paramServ3}.{paramServ4}"";//poner el Ip del servidor
+
+            IPAddress staticIP({paramIp1}, {paramIp2}, {paramIp3}, paramIp4); // Dirección IP fija deseada
+            IPAddress gateway({paramGate1}, {paramGate2}, {paramGate3}, {paramGate4});    // Dirección IP de tu router
+            IPAddress subnet({paramMask1}, {paramMask2}, {paramMask3}, {paramMask4});   // Máscara de subred
+
+
+            /*** Variables para Humedad y Temperatura ****/
+            float temperatura = 0;
+            float progresiva = {paramKm}.{paramKmDec};
+            String url;
+
+            #define DS18B20 5 //DS18B20 esta conectado al pin GPIO D5 del NodeMCU
+
+            WiFiClient client;
+            AsyncWebServer server({paramPortServer});
+            OneWire ourWire(DS18B20);// Se declara un objeto para la libreria
+            DallasTemperature sensor(&ourWire);// Se declara un objeto para la otra libreria
+           
+            
+            ";
+           
+
+          
+
+
+            // Reemplazar ambos valores de delay con el tiempo seleccionado
+            string updatedCode = generator.UpdateParamInCPP(cppCode,
+                "{nameNetwork}", nameNetwork, 
+                "{passNetwork}", passNetwork,
+                "{ip1}", ip1,
+                "{ip2}", ip2,
+                "{ip3}", ip3,
+                "{ip4}", ip4,
+                "{mask1}", mask1,
+                "{mask2}", mask2,
+                "{mask3}", mask3,
+                "{mask4}", mask4,
+                "{gate1}", gate1,
+                "{gate2}", gate2,
+                "{gate3}", gate3,
+                "{gate4}", gate4,
+                "{server1}", server1,
+                "{server2}", server2,
+                "{server3}", server3,
+                "{server4}", server4,
+                "{portServer}", portServer,
+                "{km}", km,
+                "{kmDec}", kmDec
+                );
+
+
+            string folderName = "Prueba";
+            string folderPath = Path.Combine("C:\\Users\\b-j-e\\Desktop", folderName);
+
+            Directory.CreateDirectory(folderPath);
+
+            // Directorio donde se guardara el archivo .ino
+            string filePath = "C:\\Users\\b-j-e\\Desktop\\Prueba\\Prueba.ino";
+
+            //Generador del archivo .ino
+            generator.GenerateINOFile(filePath, updatedCode);
+
+            MessageBox.Show("Archivo .ino generado correctamente.");
+        }
+
+    }
+
+    public class ArduinoINOGenerator
+    {
+        public string UpdateParamInCPP(string cppCode, 
+            string paramNameNetwork, string valueNameNetwork, 
+            string paramPassNetwork, string valuePassNetwork,
+            string paramIp1, string valueIp1,
+            string paramIp2, string valueIp2,
+            string paramIp3, string valueIp3,
+            string paramIp4, string valueIp4,
+            string paramMask1, string valueMask1,
+            string paramMask2, string valueMask2,
+            string paramMask3, string valueMask3,
+            string paramMask4, string valueMask4,
+            string paramGate1, string valueGate1,
+            string paramGate2, string valueGate2,
+            string paramGate3, string valueGate3,
+            string paramGate4, string valueGate4,
+            string paramServer1, string valueServer1,
+            string paramServer2, string valueServer2,
+            string paramServer3, string valueServer3,
+            string paramServer4, string valueServer4,
+            string paramPortServer, string valuePortServer,
+            string paramKm, string valueKm,
+            string paramKmDec, string valueKmDec
+            )
+        {
+            // Reemplazar el marcador con el valor proporcionado
+            string updatedCode = cppCode.Replace(paramNameNetwork, valueNameNetwork)
+                                        .Replace(paramPassNetwork, valuePassNetwork)
+                                        .Replace(paramIp1, valueIp1)
+                                        .Replace(paramIp2, valueIp2)
+                                        .Replace(paramIp3, valueIp3)
+                                        .Replace(paramIp4, valueIp4)
+                                        .Replace(paramMask1, valueMask1)
+                                        .Replace(paramMask2, valueMask2)
+                                        .Replace(paramMask3, valueMask3)
+                                        .Replace(paramMask4, valueMask4)
+                                        .Replace(paramGate1, valueGate1)
+                                        .Replace(paramGate2, valueGate2)
+                                        .Replace(paramGate3, valueGate3)
+                                        .Replace(paramGate4, valueGate4)
+                                        .Replace(paramServer1, valueServer1)
+                                        .Replace(paramServer2, valueServer2)
+                                        .Replace(paramServer3, valueServer3)
+                                        .Replace(paramServer4, valueServer4)
+                                        .Replace(paramPortServer, valuePortServer)
+                                        .Replace(paramKm, valueKm)
+                                        .Replace(paramKmDec, valueKmDec)
+                                        ;
+
+            return updatedCode;
+        }
+
+        public void GenerateINOFile(string filePath, string cppCode)
+        {
+            // Crea un nuevo archivo .ino
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                // Escribe el contenido del archivo .ino
+                writer.WriteLine("// Código .ino generado por C#");
+                writer.WriteLine();
+
+                writer.WriteLine("#include <ESP8266WiFi.h>");
+                writer.WriteLine("#include <ESPAsyncWebServer.h>");
+                writer.WriteLine("#include <OneWire.h>");
+                writer.WriteLine("#include <DallasTemperature.h>");
+
+                writer.WriteLine();
+
+                writer.WriteLine(cppCode);
+            }
+        }
+
+       
     }
 }
